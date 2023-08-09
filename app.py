217ae4b17de2830c409ce6f2bdffb58b3a54a47e
Soo kyung choi <96818204+teresa881016@users.noreply.github.com>
2023-08-09 20:56:01 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,95 +1,54 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-from bson.objectid import ObjectId
-from pymongo import MongoClient
-from math import ceil
-from urllib.parse import unquote, quote
-# Flask session 객체는 기본적으로 사용자의 웹 브라우저에 쿠키 형태로 데이터를 저장하여 상태를 유지합니다.
-# 이를 활용하면 각 사용자마다 로그인 상태를 감지하고, 로그인 상태에 따라 다른 페이지로 이동하거나 로그인 정보를 유지할 수 있습니다.
-
+from flask import Flask, render_template, request, jsonify, flash
 app = Flask(__name__)
-app.secret_key = 'secret_key'
 
-# 사용자 정보를 딕셔너리로 관리하는 대신에 MongoDB를 활용하겠습니다.
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.bko1xj3.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
 
-# MongoDB 연결 설정
-client = MongoClient('mongodb+srv://sparta:test@cluster0.vekhvil.mongodb.net/?retryWrites=true&w=majority')
-db = client.EPLgallary
 
-# 회원 정보를 DB에 저장하고 조회하는 함수들을 정의합니다.
-def save_user_info(user_id, user_pw, user_nickname):
-    db.Login.insert_one({'usrId': user_id, 'password': user_pw, 'nickname': user_nickname})
-
-def check_user_info(user_id):
-    return db.Login.find_one({'usrId': user_id})
-
-def get_user_nickname(user_id):
-    user_info = db.Login.find_one({'usrId': user_id}, {'nickname': True})
-    return user_info['nickname'] if user_info else None
-
-# 홈 페이지
-#  'if 'user_id' in session:'이 부분은 세션에 user_id가 있는지 확인하는 부분입니다.
-#  로그인 상태를 판단하기 위해 사용합니다.
-#  만약 세션에 user_id가 있다면, 사용자가 로그인한 상태라고 간주하고 홈 페이지로 리디렉션합니다.
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    return render_template('join.html')
 
-    return render_template('home.html')
+@app.route("/join", methods=["POST", "GET"])
+def join_post():
+    if request.method == "GET":
+        return render_template("join.html")
+    else:
+        name_receive = request.form['name_give']
+        id_receive = request.form['id_give']
+        pw_receive = request.form['pw_give']
+        pw2_receive = request.form['pw2_give']
+        nickname_receive = request.form['nickname_give']
 
+        # 빈칸 있는지 확인하기
+        if not(name_receive and id_receive and pw_receive and pw2_receive and nickname_receive):
+            error_msg = "입력되지 않은 정보가 있습니다."
+            return render_template("join.html", error_msg=error_msg)
 
-# 로그인 페이지
-# session['user_id'] = user_id: 이 부분은 로그인이 성공했을 때, 세션에 user_id를 저장하는 부분입니다.
-# 이렇게 하면 앞으로 사용자가 로그인한 상태를 유지할 수 있습니다.
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if 'user_id' in session:
-        return redirect(url_for('home'))
+        
+        # 비밀번호 일치 여부 확인하기
+        elif pw_receive != pw2_receive:
+            error_msg =  "비밀번호가 일치하지 않습니다."
+            return render_template("join.html", error_msg=error_msg)
 
-    if request.method == 'POST':
-        user_id = request.form['user_id']
-        user_pw = request.form['user_pw']
-
-        # 사용자 정보를 DB에서 조회하여 확인합니다.
-        user_info = check_user_info(user_id)
-
-        if user_info and user_info['password'] == user_pw:
-            session['user_id'] = user_id
-            session['user_nickname'] = user_info['nickname']  # 닉네임 정보를 세션에 저장
-            return redirect(url_for('home'))
         else:
-            error_message = "로그인 실패!"
-            return render_template('login.html', error_message=error_message)
+            # 이상 없으면 다음과 같은 값을 DB에 저장하기
+            doc = {
+                'name': name_receive, 
+                'id' : id_receive,
+                'pw' : pw_receive,
+                'pw2' : pw2_receive,
+                'nickname' : nickname_receive   
+            }
+            db.join.insert_one(doc)
+            return jsonify({'msg': '회원가입이 되었습니다.'})
 
-    return render_template('login.html')
-
-# 회원가입 페이지
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        user_id = request.form['user_id']
-        user_pw = request.form['user_pw']
-        user_nickname = request.form['user_nickname']
-
-        # 이미 등록된 아이디인지 확인합니다.
-        if check_user_info(user_id):
-            error_message = "이미 등록된 아이디입니다."
-            return render_template('register.html', error_message=error_message)
-
-        # 회원 정보를 DB에 저장합니다.
-        save_user_info(user_id, user_pw, user_nickname)
-        return redirect(url_for('login'))
-
-    return render_template('register.html')
-
-# 로그아웃 페이지
-# session.pop('user_id', None): 이 부분은 로그아웃 시 세션에서 user_id를 삭제하는 부분입니다.
-#  로그아웃을 수행하면 세션에서 user_id 정보가 삭제되며, 사용자는 다시 로그인해야 합니다.
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('home'))
-
+    
+@app.route("/join", methods=["GET"])
+def login_get():
+    all_buckets = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'result': all_buckets })
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
